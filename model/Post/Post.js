4ce5610ccfd10fa8246498798df54a5829bab863
Sayantan Sadhu <dip.sadhu111@gmail.com>
2024-07-29 16:48:31 +0530

    work

--- model/Post/Post.js
+++ model/Post/Post.js
@@ -1,56 +1,57 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: [true, "Post Title is required"],
-        trim: true
+      type: String,
+      required: [true, "Post Title is required"],
+      trim: true,
     },
     description: {
-        type: String,
-        required: [true, "Post Description is required"],
+      type: String,
+      required: [true, "Post description is required"],
     },
     category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Category",
-        // required: [true, "Post category is required"],
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Post category is required"],
     },
     numViews: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        }
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
     ],
     likes: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        }
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
     ],
-    dislikes: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        }
+    disLikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
     ],
+
     comments: [
       {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Comment"
-      }
-  ],
-    user: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: [true, "Please Author is required"]
-        }
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
     ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please Author is required"],
+    },
     photo: {
-        type: String,
-        // required: [true, "Post Image is required"]
-    }
-}, {
+      type: String,
+      required: [true, "Post Image is required"],
+    },
+  },
+  {
     timestamps: true,
     toJSON: { virtuals: true },
   }

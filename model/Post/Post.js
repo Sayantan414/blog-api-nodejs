b672c9f4b851ceebf6d169a425efdbe530c570a1
Sayantan Sadhu <dip.sadhu111@gmail.com>
2024-08-24 11:59:10 +0530

    work

--- model/Post/Post.js
+++ model/Post/Post.js
@@ -48,7 +48,7 @@ const postSchema = new mongoose.Schema(
     },
     photo: {
       type: String,
-      required: [true, "Post Image is required"],
+      // required: [true, "Post Image is required"],
     },
   },
   {
